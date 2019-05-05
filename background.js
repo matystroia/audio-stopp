6f280cf908ad956d3d81fd7ba0763cc2ce7f0c5b
Matei <stroiamatei@gmail.com>
2019-05-05 12:05:58 +0300

    horrible icons

--- background.js
+++ background.js
@@ -1,19 +1,20 @@
-  function changeTabNumber() {
-    var querying = browser.tabs.query({audible: true});
-    querying.then(changeBadge, onError);
-  }
+let index = 0;
 
-  function changeBadge(tabs) {
-    var ntabs = tabs.length;
-    browser.tabs.update(tabs[(++index) % ntabs].id, {active: true});
+function changeTabNumber() {
+  browser.tabs.query({audible: true}).then(changeBadge, onError);
+}
 
-    browser.browserAction.setBadgeBackgroundColor({color: "rgb(128,43,53)"});
-    browser.browserAction.setBadgeText({text: ntabs.toString()});
-  }
-  
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-  
-  var index = 0;
-  browser.browserAction.onClicked.addListener(changeTabNumber);
+function changeBadge(tabs) {
+  const nTabs = tabs.length;
+  browser.tabs.update(tabs[(++index) % nTabs].id, {active: true});
+
+  browser.browserAction.setBadgeBackgroundColor({color: "rgb(128,43,53)"});
+  browser.browserAction.setBadgeText({text: nTabs.toString()});
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+
+browser.browserAction.onClicked.addListener(changeTabNumber);
